2fb618ba955c77ae94d27d954c9d47889d14c010
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-07-17 20:28:20 +0530

    Action changes, modified more material icons

--- zenith/core/customFileSystemModel.py
+++ zenith/core/customFileSystemModel.py
@@ -33,13 +33,30 @@ class CustomFileSystemModel(QFileSystemModel):
 
     def getCustomIcon(self, file_info):
 
+        # File icons based on file name
+        file_name = file_info.fileName().lower()
+        if file_name == "license":
+            return self.license
+        if file_name == "vercel.json":
+            return self.vercel
+        if file_name == "dockerfile":
+            return self.docker
+        if file_name == "gulpfile.js":
+            return self.gulp
+        if file_name == "package.json":
+            return self.nodejs
+        if file_name == "package-lock.json":
+            return self.nodejs_alt
+        if file_name in ["bun.lockb", "bun.lock", "bun"]:
+            return self.bun
+
         # Folder icons based on folder name
         if file_info.isDir():
             if file_info.fileName() == ".github":
                 return self.github_folder
             if file_info.fileName() in ["__pycache__", ".mypy_cache"]:
                 return self.python_folder
-            if file_info.fileName() == "scripts":
+            if file_info.fileName().lower() in ["scripts", "bin"]:
                 return self.scripts_folder
             if file_info.fileName() in [
                 ".venv",
@@ -82,6 +99,41 @@ class CustomFileSystemModel(QFileSystemModel):
                 return self.framework_folder
             if file_info.fileName() in ["lexer", "syntax", "lexers"]:
                 return self.lexer_folder
+            if file_info.fileName() in ["utils", "utilities"]:
+                return self.utils_folder
+            if file_info.fileName() in ["src", "source"]:
+                return self.src_folder
+            if file_info.fileName().lower() in ["include", "includes"]:
+                return self.include_folder
+            if file_info.fileName().lower() in ["lib", "libs"]:
+                return self.lib_folder
+            if file_info.fileName() in ["app", "application"]:
+                return self.folder_app
+            if file_info.fileName() in ["pages", "views"]:
+                return self.folder_pages
+            if file_info.fileName() in ["css", "styles"]:
+                return self.folder_css
+            if file_info.fileName() in ["js", "javascript"]:
+                return self.folder_js
+            if file_info.fileName() in ["sass", "scss"]:
+                return self.folder_sass
+            if file_info.fileName() in ["node", "node_modules"]:
+                return self.folder_node
+            if file_info.fileName() in [
+                "dist",
+                "build",
+                "bin",
+                "out",
+                "output",
+                ".bin",
+            ]:
+                return self.folder_dist
+            if file_info.fileName() in ["pdf", "pdfs"]:
+                return self.folder_pdf
+            if file_info.fileName() in ["fonts", "font"]:
+                return self.folder_font
+            if file_info.fileName() in ["resources", "res"]:
+                return self.assets
             return self.folder_icon  # Default folder icon for unknown folder names
 
         # File icons based on file extension or name
@@ -120,14 +172,25 @@ class CustomFileSystemModel(QFileSystemModel):
             if file_info.fileName() == "requirements.txt":
                 return self.python_misc
             return self.default_icon
-        elif suffix in ["toml", "ini", "cfg", "conf", "flake8"]:
+        elif suffix in ["toml", "ini", "cfg", "conf", "flake8", "config"]:
             if file_info.fileName() == "pyproject.toml":
                 return self.python_misc
             return self.config_icon
         elif suffix in ["bat", "cmd"]:
             return self.batch
+        elif suffix == "exe":
+            return self.exe
         elif suffix in ["yml", "yaml"]:
             if file_info.fileName() == "dependabot.yml":
                 return self.dependabot
             return self.yaml
+        elif suffix == "zip":
+            return self.zip
+        elif suffix == "sass":
+            return self.sass
+        elif suffix == "svg":
+            return self.svg
+        elif suffix in ["ttf", "otf", "woff", "woff2"]:
+            return self.font
+
         return self.default_icon  # Default icon for unknown file types
